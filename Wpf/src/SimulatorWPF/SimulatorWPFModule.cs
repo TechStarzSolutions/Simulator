2c86649ed46747b7330a8b55ad77f44a29c7dee1
Shifanila <shifanila@techstarzsolutions.com>
2024-02-20 15:06:14 +0530

    Wpf commit

---
+++ Wpf/src/SimulatorWPF/SimulatorWPFModule.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace SimulatorWPF;
+
+[DependsOn(typeof(AbpAutofacModule))]
+public class SimulatorWPFModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        context.Services.AddSingleton<MainWindow>();
+    }
+}
